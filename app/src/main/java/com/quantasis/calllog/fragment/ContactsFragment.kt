17920d7db8277934bf0fa9f1a642b494a7f4e438
Kanaiya Katarmal <kanaiya@quantasis.com>
2025-06-08 13:43:25 +0530

    WIP

--- app/src/main/java/com/quantasis/calllog/fragment/ContactsFragment.kt
+++ app/src/main/java/com/quantasis/calllog/fragment/ContactsFragment.kt
@@ -2,10 +2,12 @@ package com.quantasis.calllog.fragment
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.SearchView
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -29,16 +31,16 @@ class ContactsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerView: RecyclerView = view.findViewById(R.id.recycler_view_contacts)
-        val searchView: SearchView = view.findViewById(R.id.search_view)
+        val searchEditText: EditText = view.findViewById(R.id.searchEditText)
 
-        adapter = ContactCallInfoAdapter{ contactInfo ->
-
+        adapter = ContactCallInfoAdapter { contactInfo ->
             val intent = Intent(requireContext(), CallerDashboardActivity::class.java).apply {
                 putExtra("name", contactInfo.contactName)
                 putExtra("number", contactInfo.phone)
             }
             startActivity(intent)
         }
+
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
 
@@ -48,16 +50,14 @@ class ContactsFragment : Fragment() {
             adapter.submitList(it)
         }
 
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                query?.let { viewModel.setSearchQuery(it) }
-                return true
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.setSearchQuery(s?.toString() ?: "")
             }
 
-            override fun onQueryTextChange(newText: String?): Boolean {
-                viewModel.setSearchQuery(newText ?: "")
-                return true
-            }
+            override fun afterTextChanged(s: Editable?) {}
         })
     }
 }
