924ed431904646a2bbbe12e0536c86f99fcd007d
Kanaiya Katarmal <kanaiya@quantasis.com>
2025-06-15 15:23:37 +0530

    WIP

--- app/src/main/java/com/quantasis/calllog/fragment/AnalysisSummaryFragment.kt
+++ app/src/main/java/com/quantasis/calllog/fragment/AnalysisSummaryFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.quantasis.calllog.R
@@ -32,7 +33,7 @@ class AnalysisSummaryFragment : Fragment() {
 
         adapter = AnalysisSummaryAdapter()
         val recyclerView = view.findViewById<RecyclerView>(R.id.summaryRecyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
         recyclerView.adapter = adapter
 
         // Replace with actual values
